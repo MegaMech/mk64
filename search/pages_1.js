21b7ef26e2d299c5cde7d65034f0fbba693ab3e1
MegaMech <MegaMech@users.noreply.github.com>
2024-01-20 15:44:08 +0000

    Deploying to gh-pages from @ MegaMech/mk64@1c2b95f383290895610bf71872b6c2a9df9017f0 🚀

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['basic_20concepts_29128',['Basic Concepts',['../concepts.html',1,'basics']]],
-  ['boostramps_29129',['Boostramps',['../boostramps.html',1,'tutorials']]],
-  ['bug_20list_29130',['Bug List',['../bug.html',1,'']]]
+  ['basic_20concepts_29132',['Basic Concepts',['../concepts.html',1,'basics']]],
+  ['boost_20ramps_29133',['Boost Ramps',['../boostramps.html',1,'tutorials']]],
+  ['bug_20list_29134',['Bug List',['../bug.html',1,'']]]
 ];
--- search/pages_2.js
+++ search/pages_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['compiling_20for_20windows_29131',['Compiling for Windows',['../buildwindows.html',1,'basics']]],
-  ['control_20flow_29132',['Control Flow',['../controlflow.html',1,'basics']]],
-  ['courses_29133',['Courses',['../courses.html',1,'coursesmenu'],['../coursesmenu.html',1,'(Global Namespace)']]]
+  ['codebase_20overview_29135',['Codebase Overview',['../controlflow.html',1,'basics']]],
+  ['compiling_20for_20windows_29136',['Compiling for Windows',['../buildwindows.html',1,'basics']]],
+  ['courses_29137',['Courses',['../courses.html',1,'coursesmenu'],['../coursesmenu.html',1,'(Global Namespace)']]]
 ];
